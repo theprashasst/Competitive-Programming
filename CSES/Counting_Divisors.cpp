646fbf35f6e5834291d5ab6daa3ad61c1cc11d43
Prashasst Dongre <prashassst@gmail.com>
2024-06-20 16:15:53 +0530

    1100* rating que solved
    first 1100* rating question solved

---
+++ CSES/Counting_Divisors.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define range(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define input(n, arr) for(int i = 0; i < (n); ++i) cin >> (arr)[i];
+
+const int INF = 1<<29;
+using ll= long long;
+
+
+void solve(){
+    int n;cin>>n;
+   int ans=0;
+   for (int i=1; i<=n/i; i++){
+    if (n%i==0) ans+= (i==n/i) ? 1 : 2;
+   }
+   cout<<ans;
+   
+
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+  int t=1;
+  cin>>t;
+  for(int i=0;i<t;i++){
+    //cout << fixed << setprecision(10);
+    solve();
+    cout<<endl;
+  }
+}
